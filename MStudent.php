6b2fd7bf70c4d16bf33a289c459c6e3d2347e5d8
Christoffer Kjeldgaard <chris.kjeldgaard@gmail.com>
2013-03-21 13:06:27 +0100

    first version of MStudent and mysqlhandler

---
+++ MStudent.php
@@ -0,0 +1,168 @@
+<?php
+
+require 'mysqlhandler.php';
+class MStudent{
+	/* ----- TODO ------
+	 * function for error handling.    
+	 */
+	public $name;
+	public $ID;
+	public $email;
+	public $skype;
+	public $mobil;
+	public $isLoaded;
+	
+	/*
+	 * construct does nothing at the moment. use create() or load() instead. 
+	 */
+	function __construct()
+	{
+		$this->isLoaded = FALSE;
+	}
+	
+	
+	/*
+	 * Creates new student and inserts it into mySQL database. 
+	 * ---PARAMS
+	 * name:	student name (string)
+	 * mobil:	student cell (string)
+	 * email:	student email (string)
+	 * skype:	student skype contact info (string)
+	 */
+	function create($name, $mobil, $email, $skype){
+		if(!$isLoaded){
+			$db = new MySQLHandler('localhost', 'root', 'jyh69egz', 'ProjectAdministrationTool');
+			$db->multiquery($db, 'INSERT INTO student (studentName, email, mobil, skype) 
+							      VALUES ("'.$name.'", "'.$email.'", "'.$mobil.'", "'.$skype.'")');
+			$db->close();
+			$this->name = $name; $this->email = $email; $this->mobil = $mobil; $this->skype = $skype; $this->isLoaded = TRUE;}
+	}
+	
+	/*
+	 * loads a student from db. 
+	 * ---PARAMS
+	 * ID:	valid studentID. 
+	 * ---RETURNS
+	 * a student 
+	 */
+	function load($id){
+		$db = new MySQLHandler('localhost', 'root', 'jyh69egz', 'ProjectAdministrationTool');
+		$row = $db->multiquery($db, "SELECT studentName, email, mobil, skype FROM student WHERE studentID = $id");
+		$db->close();
+		//LOAD parameters into student object.
+		if($row){
+			$this->ID = $id; $this->name = $row[0]; $this->email = $row[1]; $this->mobil = $row[2]; $this->skype = $row[3];
+			$this->isLoaded = TRUE;
+			$this->printer($row[0], $row[1], $row[2], $row[3]);
+			return "success";
+		}
+		print "error: invalid ID"; 
+	}
+	
+	/*
+	 * getID - returns studentID, if found.  
+	 * ---PARAMS
+	 * key:		search parameter (string)
+	 * value:	search string (string)
+	 * ---RETURNS
+	 * resultset from db->store_result() - or "error" if the resultset is empty.
+	 */
+	function getID($key, $value){
+		//key is search parameter.
+		$db = new MySQLHandler('localhost', 'root', 'jyh69egz', 'ProjectAdministrationTool');
+		if ($key == "name"){
+			$row = $db->multiquery($db, 'SELECT studentID FROM student WHERE studentName = "'.$value.'"');}
+			
+		if ($key == "email"){
+			$row = $db->multiquery($db, 'SELECT studentID FROM student WHERE email = "'.$value.'"');}
+			
+		if ($key == "mobil"){
+			$row = $db->multiquery($db, 'SELECT studentID FROM student WHERE mobil = "'.$value.'"');}
+			
+		if ($key == "skype"){
+			$row = $db->multiquery($db, 'SELECT studentID FROM student WHERE skype = "'.$value.'"');}
+			
+		$db->close();
+		if ($row){
+			return $row;
+		}
+		print("error:" . $db->error);
+		
+	}
+	
+   /*
+	* updateSkype - updates Skype, if this->isLoaded is TRUE.
+	* ---PARAMS
+	* $id:		studentID from db - see getID to fetch ID from db.
+	* $skype:	new skype value.
+	*/
+	function updateSkype($id, $skype){
+		if(!$this->isLoaded){
+			$this->load($id);}
+		
+		if($this->isLoaded){
+			$db = new MySQLHandler('localhost', 'root', 'jyh69egz', 'ProjectAdministrationTool');
+			$db->multiquery($db, "UPDATE student SET skype = '$skype' WHERE studentID = $id");
+			$this->skype = $skype;}
+	}
+	
+   /*
+	* updateEmail - updates email address, if this->isLoaded is TRUE.
+	* ---PARAMS
+	* $id:		studentID from db - see getID to fetch ID from db.
+	* $email:	new email value.
+	*/
+	function updateEmail($id, $email){
+		if(!$this->isLoaded){
+			$this->load($id);}
+		
+		if($this->isLoaded){
+			$db = new MySQLHandler('localhost', 'root', 'jyh69egz', 'ProjectAdministrationTool');
+			$db->multiquery($db, "UPDATE student SET email = '$email' WHERE studentID = $id");
+			$this->email = $email;}
+	}
+	
+	
+   /*
+	* updateMobil - updates mobil, if this->isLoaded is TRUE.
+	* ---PARAMS
+	* $id:		studentID from db - see getID to fetch ID from db.
+	* $email:	new mobil value.
+	*/
+	function updateMobil($id, $mobil){
+		if(!$this->isLoaded){
+			$this->load($id);}
+			
+		if($this->isLoaded){
+			$db = new MySQLHandler('localhost', 'root', 'jyh69egz', 'ProjectAdministrationTool');
+			$db->multiquery($db, "UPDATE student SET mobil = '$mobil' WHERE studentID = $id");
+			$this->mobil = $mobil;}
+	}
+
+   /*
+	* updateName - updates name, if this->isLoaded is TRUE.
+	* ---PARAMS
+	* $id:		studentID from db - see getID to fetch ID from db.
+	* $name:	new name value.
+	*/
+	function updateName($id,$name){
+		if(!$this->isLoaded){
+			$this->load($id);}
+		
+		if($this->isLoaded){
+			$db = new MySQLHandler('localhost', 'root', 'jyh69egz', 'ProjectAdministrationTool');
+			$db->multiquery($db, "UPDATE student SET studentName = '$name' WHERE studentID = $id");
+			$this->name = $name;}
+	}
+	
+	/*printer - prints out student --- DEBUGGING ONLY!
+	 * 
+	 */
+	function printer($name,$email,$mobil,$skype){
+		print "Student: $name, $email, $mobil, $skype";}
+}
+
+/* ---------TESTING AREA---------*/
+/*------PUT unittests here!------*/
+
+?>
---
+++ mysqlhandler.php
@@ -0,0 +1,86 @@
+<?php
+
+/*Created by Christoffer Kjeldgaard, all rights reserved. 
+ * 
+ *------------------HOW TO USE MYSQLHANDLER.PHP---------------- 
+ *__construct: opens connection to MySQL server. 
+ *function multiquery: Performs multiquery on database.
+ *function printer: prints out whatever is in row[pos]. 
+ *function close: closes connection. 
+ *
+ *-------------------------Examples----------------------------
+ *$db = new MySQLHandler('host', 'user', 'password', 'database');
+ *$result = $db->multiquery($db, "SELECT * from student;");
+ *$db->close();
+ */
+Class MySQLHandler extends mysqli {
+
+	public function __construct($host,$user,$passw,$db) {
+		parent::__construct($host,$user,$passw,$db);
+		
+		if (mysqli_connect_error()) {
+			die('Connect error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+			echo "Error occured!";
+		}
+	
+	}
+	/*
+	 *multiquery
+	 *--PARAMS
+	 *db:		valid mySQL connection.
+	 *query:	one or more string queries separated by ;
+	 *
+	 *--RETURN
+	 *resultset from db->store_result();	 
+	 */
+	
+	public function multiquery($db, $query) {
+		//remember to connect first!! or things will go bad. 
+		//Perform multi query - remember that each query must be seperated by ';', otherwise things will go bad.   
+		$db->multi_query($query);
+		$result = $db->store_result();
+		if($result){
+			$row = $result->fetch_row();}	
+	    return $row;
+		
+	}	
+	
+	/*
+	 * printer
+	 * --PARAMS
+	 * db: 		active, and valid mySQL connection. 
+	 * result:	resultset from db->store_result();
+	 * pos:		coloumn position (int), that should be printed. 
+	 * 
+	 * --RETURNS
+	 * nothing
+	 */
+	public function printer($db, $result, $pos){
+		do {		
+			//check that the result is valid. This could be done externally in controller. Should probably be moved out
+			if($result) {
+				//Result is valid. lets get the rows.
+				while($row = $result->fetch_row()){
+					//This while runs as long as there is still rows to get from the current query.
+					//do something with the row here. eg print it with:
+					printf("%s\n", $row[$pos]);
+				}
+				//Now there is no more rows to handle. lets free the result to make room for the next result:
+				$result->free();
+			}
+			//now lets make sure to seperate the queries from eachother. We do this by checking more_results()
+			if($result->more_results()){
+				printf("---------\n");
+			}
+		} while($db->next_result());
+	}
+	
+	/*
+	 * closes connection. 
+	 */
+	public function close()
+	{
+		parent::close();
+	}
+}
+?>
